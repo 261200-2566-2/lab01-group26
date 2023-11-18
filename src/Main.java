e6f3bcf1c4a789e1de82bfabd4156b30151a7bb4
PremerX007 <bannawat_55120@outlook.com>
2023-11-18 12:26:56 +0700

    code finished but non-recheck

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo!!");
+        Scanner s = new Scanner(System.in);
+        int n = CheckN(s);
+
+        int[] array = new int[n];
+
+        for(int i=0; i<n; i++){
+            array[i] = s.nextInt();
+        }
+
+        Sorter(array);
+        DisplayData(array);
+
     }
+    public static void DisplayData(int[] arr){
+        for (int data : arr){
+            System.out.print(data + " ");
+        }
+    }
+    public static void Sorter(int[] arr){
+        Arrays.sort(arr);
+    }
+
+    public static int CheckN(Scanner s){
+        int c = s.nextInt();
+        while(c < 0){
+            System.out.println("Value must be greater than or equal to 0");
+            c = s.nextInt();
+        }
+        return c;
+    }
+
 }
